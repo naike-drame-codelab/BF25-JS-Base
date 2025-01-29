7613f636af08fda365470b1917c643a35c02b287
Naïké Dramé <naike.drame.dev@gmail.com>
2025-01-29 11:44:14 +0100

    added demo Calculate BMI

---
+++ BMI/bmi.js
@@ -0,0 +1,21 @@
+// DOM Elements
+// pour récupérer les valeurs
+const heightInput = document.getElementById("heightInput");
+const weightInput = document.getElementById("weightInput");
+// pour récupérer un événement click
+const calculateButton = document.getElementById("calculateButton");
+// pour afficher le résultat
+const resultDiv = document.getElementById("result");
+
+// fonction qui sera exécutée au click
+calculateButton.addEventListener("click", () => {
+  // poids / (tailleEnMetre * tailleEnMetre)
+  console.log("clicked");
+
+  const w = weightInput.valueAsNumber;
+  const h = heightInput.valueAsNumber / 100;
+  const bmi = w / h ** 2;
+  console.log(bmi);
+
+  resultDiv.textContent = `Votre BMI est de ${bmi.toFixed(2)}.`;
+});
