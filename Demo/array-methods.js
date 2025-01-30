309f0f54455c269f948730ecc7a2d2a959dbd8f0
Naïké Dramé <naike.drame.dev@gmail.com>
2025-01-30 15:54:06 +0100

    demo array methods

---
+++ Demo/array-methods.js
@@ -0,0 +1,26 @@
+// .sort()
+
+const tab = [3, 2, 1, 6, 7, 8, -1, 2, 7, 42, -3];
+
+tab.sort((a, b) => a - b);
+// tab.sort((a, b) => (a < b ? -1 : 1));
+
+console.log(tab); // [  -3, -1, 1, 2, 2,  3,  6, 7, 7, 8, 42]
+
+const tab2 = [...tab];
+tab2.sort((a, b) => b - a);
+console.log(tab2); // [42, 8, 7, 7,  6, 3, 2, 2, 1, -1, -3]
+
+const mots = ["maison", "àrbre", "Paquebot", "biberon"];
+// mots.sort((a, b) => (a < b ? -1 : 1)); // tri sur ASCII : [ 'Paquebot', 'biberon', 'maison', 'àrbre' ]
+// console.log(mots);
+const mots2 = [...mots];
+mots.sort((a, b) => a.localeCompare(b));
+console.log(mots); // [ 'àrbre', 'biberon', 'maison', 'Paquebot' ]
+mots2.sort((a, b) => b.localeCompare(a));
+console.log(mots2); // [ 'Paquebot', 'maison', 'biberon', 'àrbre' ]
+
+// .some() = trouve si l'array contient une valeur sur base de la méthode de test
+// .every() = trouve si l'array contient toutes les valeurs sur base de la méthode de test
+// .find() / .findLast() = trouve 1er elem répondant à la condition
+//.filter() = trouve tous les elems répondant à la condition
