943be697a1648c4db81f7b1997980fc581a80098
Naïké Dramé <naike.drame.dev@gmail.com>
2025-01-30 15:58:33 +0100

    added equivalence c#

--- Demo/array-methods.js
+++ Demo/array-methods.js
@@ -20,7 +20,20 @@ console.log(mots); // [ 'àrbre', 'biberon', 'maison', 'Paquebot' ]
 mots2.sort((a, b) => b.localeCompare(a));
 console.log(mots2); // [ 'Paquebot', 'maison', 'biberon', 'àrbre' ]
 
+const t1 = ["Khun", "Mike"];
+const t2 = ["Thierry", "Aurelien"];
+const t3 = [...t1, ...t2];
+
 // .some() = trouve si l'array contient une valeur sur base de la méthode de test
 // .every() = trouve si l'array contient toutes les valeurs sur base de la méthode de test
 // .find() / .findLast() = trouve 1er elem répondant à la condition
 //.filter() = trouve tous les elems répondant à la condition
+
+//----- js => c# -----
+// map => Select
+// find => Find
+// filter => Where
+// some => Any
+// every => All
+// toSorted => OrderBy // OrderByDescending
+// reduce => GroupBy // Sum // Average
